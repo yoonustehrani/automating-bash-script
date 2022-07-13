666ee16eccc5d795f73300f739970c0642dd04a2
yoonus <yoonustehrani28@gmail.com>
2022-07-13 16:04:38 +0430

    *Feature: Customizing the timer mode and timer value now available

--- create-service.sh
+++ create-service.sh
@@ -55,29 +55,88 @@ check_user_or_create $user
 shared_service_dir=/lib/systemd/system
 serviced_path=$shared_service_dir/$service.service
 
+create_service_file() {
+    sed "s_\${scriptpath}_${script}_" ./service.service.stub \
+    | sed "s_\${workdir}_$(realpath .)_" \
+    | sed "s#\${username}#${user}#g" \
+    | sed "s_\${usergroup}_${user}_" \
+    | sed "s_\${logdir}_/var/log/memchecker_" \
+    | sed "s_\${service}_${service}_" \
+    | sudo tee $serviced_path > /dev/null
+}
 # making service file from stub
 echo "+ Creating service file ..."
-sed "s_\${scriptpath}_${script_path}_" ./service.service.stub \
-| sed "s_\${workdir}_$(realpath .)_" \
-| sed "s#\${username}#${user}#g" \
-| sed "s_\${usergroup}_${user}_" \
-| sed "s_\${logdir}_/var/log/memchecker_" \
-| sed "s_\${service}_${service}_" \
-| sudo tee $serviced_path > /dev/null
+create_service_file
 
-echo "++ Giving desired permissions to the service file ..."
+echo "++ Giving desired permissions to the $service.service file ..."
 sudo chmod 655 $serviced_path
+
 echo "+++ Done! service file created: $serviced_path"
 
 # copiying timer file for the very service
-echo "-- Creating timer"
-sudo cp $service.timer $shared_service_dir/
+ask_user_to_input_seconds() {
+    echo -e "How often do you want the timer to run ?"
+    while true;
+    do
+        echo "(enter the number in seconds e.g. 60)" 
+        read -p "_ " seconds
+        if [ -n "$seconds" ] && [[ "$seconds" =~ ^[0-9]+$ ]]
+        then
+            echo "The script will be running every $seconds seconds." 
+            break
+        fi
+    done
+    timer_value="$seconds"
+}
+
+echo "--- Making timer file"
+
+# determining timer mode
+echo -e "Which kind of timer do you tend to use for $service.timer ?\n\t1.Unit active Seconds\t2.Timer active Seconds\n\t3.Boot seconds\t4.Calendar time (Currently not available)"
+read -p "_ " mode_for_timer
+case $mode_for_timer in
+    1)
+        timer_mode="OnUnitActiveSec"
+        ask_user_to_input_seconds
+        ;;
+    2)
+        timer_mode="OnActiveSec"
+        ask_user_to_input_seconds
+        ;;
+    3)
+        timer_mode="OnBootSec"
+        ask_user_to_input_seconds
+        ;;
+    4)
+        echo "Sorry, Calendar mode currently not available."
+        # timer_mode="OnCalendar"
+        exit 0
+        ;;
+    *)
+        echo "Active Seconds mode will be used by default."
+        timer_mode="OnUnitActiveSec"
+        ask_user_to_input_seconds
+        ;;
+esac
+
+# creating timer file
+create_timer_file() {
+    sed "s_\${service}_${service}_" ./service.timer.stub \
+    | sed "s#\${timer_mode}#${timer_mode}#g" \
+    | sed "s#\${timer_value}#${timer_value}#g" \
+    | sudo tee $shared_service_dir/$service.timer > /dev/null
+}
+
+echo "--- Creating timer file"
+create_timer_file
+
+echo "-- Giving desired permissions to the $service.timer file ..."
 sudo chmod 655 $shared_service_dir/$service.timer
-echo "- Timer created"
+
+echo "- Timer file created"
 
 echo "*** Reloading systemctl daemon ***"
 sudo systemctl daemon-reload
-
 
 echo "+ starting service $service"
 sudo systemctl enable $service.service $service.timer
